c6686817e126fd1ffa9c1ed3bdab6549ba652ee1
fodela <particleme30@gmail.com>
2022-03-02 18:10:09 +0000

    feat: add burgerbuilder component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React, { Component } from "react";
 import "./App.css";
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-
 class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Layout>
+          <h2>Testing</h2>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
   }
 }
 
